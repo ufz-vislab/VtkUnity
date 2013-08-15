10c1aaf033501c4abebce3c6df239cc6627c4736
Lars Bilke <lars.bilke@ufz.de>
2013-08-15 15:53:15 +0200

    A simple test for seeing Vtk results in editor mode.

---
+++ Assets/TestVtkRunInEditor.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestVtkRunInEditor : MonoBehaviour
+{
+	public int resolution = 8;
+	int oldResolution;
+
+	Kitware.VTK.vtkSphereSource SphereSource;
+	VtkToUnity vtkToUnity = null;
+
+	void Start ()
+	{
+		Generate();
+	}
+
+	[ContextMenu("Reset Vtk")]
+	void Reset()
+	{
+		DestroyImmediate(vtkToUnity.go);
+		vtkToUnity = null;
+	}
+
+	[ContextMenu("Generate Vtk")]
+	void Generate()
+	{
+		if(SphereSource == null)
+			SphereSource = Kitware.VTK.vtkSphereSource.New();
+		if (vtkToUnity == null)
+		{
+			vtkToUnity = new VtkToUnity(SphereSource.GetOutputPort(), "VTK Run In Editor");
+			vtkToUnity.ColorBy(Color.green);
+		}
+		SphereSource.SetPhiResolution(resolution);
+		SphereSource.SetThetaResolution(resolution);
+		SphereSource.SetRadius(1);
+		SphereSource.Update();
+
+		vtkToUnity.Update();
+	}
+	
+	void Update ()
+	{
+		if (resolution != oldResolution)
+		{
+			SphereSource.SetPhiResolution(resolution);
+			SphereSource.SetThetaResolution(resolution);
+			vtkToUnity.Update();
+			oldResolution = resolution;
+		}
+	}
+}
