b799d2d3b6056314d2d26db34198655eb8d6092b
rfrister <robbyfrister@googlemail.com>
2014-03-17 14:49:08 +0100

    - Properties support for unstructured grid - DataSetSurface Filter

---
+++ Assets/VTK/VTKFilter/VTKFilterDataSetSurface.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class VTKFilterDataSetSurface : VTKFilter 
+{
+	[HideInInspector]
+	public Kitware.VTK.vtkDataSetSurfaceFilter filter;
+	
+	protected override void OnEnable ()
+	{
+		base.InputType = VTK.FilterType.UnstructuredGrid;
+		base.OutputType = VTK.FilterType.PolyData;
+	}
+	
+	protected override Kitware.VTK.vtkAlgorithmOutput GenerateOutput(Kitware.VTK.vtkAlgorithmOutput input)
+	{
+		filter = Kitware.VTK.vtkDataSetSurfaceFilter.New ();
+		
+		filter.SetInputConnection(input);
+		
+		filter.Update ();
+		
+		return filter.GetOutputPort();
+	}
+}
